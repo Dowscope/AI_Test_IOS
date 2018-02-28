ef247cc4268138f8a35b59faae80b6b51babca4e
Timothy Dowling <timothy.dowling@me.com>
2018-02-28 14:01:43 -0500

    Initial Commit

--- AI_Test_IOS/GameViewController.swift
+++ AI_Test_IOS/GameViewController.swift
@@ -17,13 +17,12 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            let scene = GameScene(size: view.frame.size)
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
             
